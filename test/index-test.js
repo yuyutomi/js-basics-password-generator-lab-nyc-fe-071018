a07352c8ed6f29fc8d40f838825d30c91b894cb9
maxwell <github email address>
2018-04-04 21:57:50 -0400

    added test messages

--- test/index-test.js
+++ test/index-test.js
@@ -1,11 +1,12 @@
 describe("index.html", () => {
-  it("has an input form and an output ", () => {
-    expect(document.getElementsByTagName("input")[0]).toExist();
-    expect(document.getElementsByTagName("input")[1]).toExist();
-    expect(document.querySelector("output")).toExist();
+  it("has an input form and an valid output area ", () => {
+    expect(document.getElementsByTagName("input")[0]).toExist('No input field for password length found');
+    expect(document.getElementsByTagName("input")[1]).toExist('No submit button found');
+    expect(document.querySelector("output").innerText).toNotExist('The <output> element should start off empty');
+    expect(document.querySelector("output")).toExist('No <output> element found');
   });
 
-  it("has a working submit button ", () => {
+  it("open the browser console to see 2nd test ", () => {
     console.log('waiting 5 seconds before checking for password')
 
     let password = document.querySelector("output").innerText
@@ -25,3 +26,32 @@ describe("index.html", () => {
 
   });
 });
+// describe("Tests for secure password generation:", () => {
+//   it("index.html has an input form and an valid output area", () => {
+//     expect(document.getElementsByTagName("input")[0]).toExist();
+//     expect(document.getElementsByTagName("input")[1]).toExist();
+//     expect(document.querySelector("output").innerText).toNotExist();
+//     expect(document.querySelector("output")).toExist();
+//   });
+//
+//   it("open the browser console to see test results ", () => {
+//     console.log('waiting 5 seconds before checking for password')
+//
+//     let password = document.querySelector("output").innerText
+//     expect(password).toNotExist()
+//
+//     setTimeout(function() {
+//       password = document.querySelector("output").innerText
+//
+//       expect(password).toExist('Test Failed: No text was found in the <output> element');
+//       expect(password).toMatch(/[a-z]+/, 'Test Failed: No lowercase letters')
+//       expect(password).toMatch(/[A-Z]+/, 'Test Failed: No uppercase letters')
+//       expect(password).toMatch(/[0-9]+/, 'Test Failed: No numbers')
+//       expect(password).toMatch(/[^A-Za-z0-9]+/, 'Test Failed: No special characters')
+//
+//       console.log('Excellent! You created a secure password!')
+//
+//       }, 5000)
+//
+//   });
+// });
