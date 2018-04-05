23cb6c27c0a9035ffac4c3653fb820a44ad90a37
maxwell <github email address>
2018-04-04 21:45:22 -0400

    cleaning css

--- test/index-test.js
+++ test/index-test.js
@@ -7,23 +7,21 @@ describe("index.html", () => {
 
   it("has a working submit button ", () => {
     console.log('waiting 5 seconds before checking for password')
+
     let password = document.querySelector("output").innerText
     expect(password).toNotExist()
 
-
     setTimeout(function() {
       password = document.querySelector("output").innerText
-      expect(password).toExist('Test Failed: No text was found in the <output> element');
 
-      console.log(password);
-      console.log(typeof password);
+      expect(password).toExist('Test Failed: No text was found in the <output> element');
       expect(password).toMatch(/[a-z]+/)
       expect(password).toMatch(/[0-9]+/)
       expect(password).toMatch(/[A-Z]+/)
       expect(password).toMatch(/[^A-Za-z0-9]+/)
-      console.log('Excellent! You passed the test!')
 
-       }, 5000)
+      console.log('Excellent! You passed the test!')
+     }, 5000)
 
   });
 });
