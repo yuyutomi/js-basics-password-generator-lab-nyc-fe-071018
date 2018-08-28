a2e84bdab3ec951ce83b8c7e5108a7988d45f2a4
Yuri Tomikawa <yuritomikawa@gmail.com>
2018-08-28 22:34:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,10 +10,7 @@ window.addEventListener("load", () => {
     event.preventDefault()
     let passwordLength = document.getElementById('passwordLength').value
     // this creates an array out of the string
-    for (let i = 0; i < passwordLength)
-    
-    for (length of passwordLength) {
-      
+    for (let i = 0; i < passwordLength; i++) {
       
     }
     let password = lowercharacters[Math.floor(Math.random()* lowercharacters.length)] + uppercharacters[Math.floor(Math.random()* uppercharacters.length)] +  number[Math.floor(Math.random()* number.length)] + specialCharacters[Math.floor(Math.random()* specialCharacters.length)]
